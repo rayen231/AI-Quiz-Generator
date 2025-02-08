fa8a9753ceee5adc46bfba28abe5c2f85be4d31e
Rayen Ben Aziza <rayenbenaziza1234@gmail.com>
2025-02-08 05:37:32 +0100

    Update front.py

--- front_end/front.py
+++ front_end/front.py
@@ -3,8 +3,8 @@ import requests
 import json
 
 # Define API endpoints
-QUIZ_API_URL = "http://127.0.0.1:8000/quiz"
-CORRECT_API_URL = "http://127.0.0.1:8000/correct"
+QUIZ_API_URL = "http://44.226.145.213:8000/quiz"
+CORRECT_API_URL = "http://44.226.145.213:8000/correct"
 
 def main():
     st.title("Quiz Generator and Evaluator")
